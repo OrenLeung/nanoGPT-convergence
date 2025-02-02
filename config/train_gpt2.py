6000ac699f0057adf5f69a4c404e1e76a788a40a
Oren <47992694+OrenLeung@users.noreply.github.com>
2025-02-01 19:17:32 -0500

    Update train_gpt2.py

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -7,8 +7,8 @@ wandb_project = 'owt'
 wandb_run_name='gpt2-124M'
 
 # these make the total batch size be ~0.5M
-# 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 12 * 5
+# 60 batch size * 1024 block size * 8 GPUs = 491,520
+batch_size = 60
 block_size = 1024
 gradient_accumulation_steps = 8
 
